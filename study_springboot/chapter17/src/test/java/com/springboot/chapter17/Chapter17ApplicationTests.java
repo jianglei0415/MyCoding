c6e81aefbcf0137de67f41c70caaa46003ea10db
jianglei0415 <1299313360@qq.com>
2020-02-03 17:19:15 +0800

    【add】20200203：[study_springboot] chapter17：分布式开发——Spring Cloud【第一部分】

---
+++ study_springboot/chapter17/src/test/java/com/springboot/chapter17/Chapter17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.chapter17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
