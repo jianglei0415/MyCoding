4241b0aa71d7949d088a784042893602ae9782ba
Administrator <1299313360@qq.com>
2020-09-03 23:48:21 +0800

    【add】20200903：[leet_code] 总刷题数：1 【add】20200903：[leet_code] 类型：backtrack 题号：#51

---
+++ leet_code/src/backtrack/SolveNQueens_51.java
@@ -0,0 +1,97 @@
+package backtrack;
+
+import java.util.*;
+
+/**
+ * @author jianglei
+ * @date 2020/9/3 22:33
+ * <p>
+ * 题号：#51. N 皇后
+ * <p>
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，
+ * 并且使皇后彼此之间不能相互攻击。
+ * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，
+ * 该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ * <p>
+ * 示例：
+ * 输入：4
+ * 输出：[
+ * [".Q..",  // 解法 1
+ * "...Q",
+ * "Q...",
+ * "..Q."],
+ * <p>
+ * ["..Q.",  // 解法 2
+ * "Q...",
+ * "...Q",
+ * ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ * <p>
+ * 提示：
+ * 皇后彼此不能相互攻击，
+ * 也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。
+ */
+public class SolveNQueens_51 {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        Set<Integer> columns = new HashSet<>();
+        Set<Integer> diagonals1 = new HashSet<>();
+        Set<Integer> diagonals2 = new HashSet<>();
+        backtrack(solutions, queens, n, 0, columns, diagonals1, diagonals2);
+        return solutions;
+    }
+
+    private static void backtrack(List<List<String>> solutions, int[] queens, int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            List<String> board = generateBoard(queens, n);
+            solutions.add(board);
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (columns.contains(i)) {
+                    continue;
+                }
+
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+
+                queens[row] = i;
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+                backtrack(solutions, queens, n, row + 1, columns, diagonals1, diagonals2);
+                queens[row] = -1;
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
+            }
+        }
+    }
+
+    private static List<String> generateBoard(int[] queens, int n) {
+        List<String> board = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            board.add(new String(row));
+        }
+        return board;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<List<String>> lists = solveNQueens(n);
+        System.out.println(lists);
+    }
+}
