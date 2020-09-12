673f8de8d4dbdbdc34939f051ca4aab0473ca687
jianglei <1299313360@qq.com>
2020-09-12 23:37:52 +0800

    feat : 20200912 今日刷题数：2 类型：dp 题号：#309 类型：list 题号：#2

---
+++ leet_code/src/list/AddTwoNumbers_2.java
@@ -0,0 +1,59 @@
+package list;
+
+import util.ListNode;
+
+/**
+ * @author jianglei
+ * @date 2020/9/12 15:29
+ * <p>
+ * 题号：#2. 两数相加
+ * <p>
+ * 给出两个 非空 的链表用来表示两个非负的整数。
+ * 其中，它们各自的位数是按照 逆序 的方式存储的，
+ * 并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ */
+public class AddTwoNumbers_2 {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode node1 = l1, node2 = l2, curr = result;
+        int carry = 0;
+        while (node1 != null || node2 != null) {
+            int x = (node1 != null) ? node1.val : 0;
+            int y = (node2 != null) ? node2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (node1 != null) {
+                node1 = node1.next;
+            }
+            if (node2 != null) {
+                node2 = node2.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode node = addTwoNumbers(l1, l2);
+        System.out.println(node);
+    }
+}
