86f3bfed2b9984e412eecd757a0cc89b29a067a4
jianglei0415 <1299313360@qq.com>
2020-12-14 23:08:56 +0800

    feat : 20201214 今日刷题数：1 类型：hash 题号：#49

---
+++ leet_code/src/hash/GroupAnagrams_49.java
@@ -0,0 +1,61 @@
+package hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jianglei
+ * @date 2020/12/14 22:21
+ * <p>
+ * 题号：# 49. 字母异位词分组
+ * <p>
+ * 给定一个字符串数组，将字母异位词组合在一起。
+ * 字母异位词指字母相同，但排列不同的字符串。
+ * <p>
+ * 示例:
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ * <p>
+ * 说明：
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ */
+public class GroupAnagrams_49 {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] counts = new int[26];
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = groupAnagrams(strs);
+        System.out.println(lists);
+    }
+}
