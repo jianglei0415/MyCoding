8ef71cce9d7dad38cc5ed35702354cbe8768024c
jianglei0415 <1299313360@qq.com>
2021-03-14 12:50:52 +0800

    eat : 20210314 今日刷题数：1 类型：design 题号：#706

---
+++ leet_code/src/design/MyHashMap_706.java
@@ -0,0 +1,116 @@
+package design;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author jianglei
+ * @date 2021/3/14 11:46
+ * <p>
+ * 题号：#706. 设计哈希映射
+ * <p>
+ * 不使用任何内建的哈希表库设计一个哈希映射（HashMap）。
+ * 实现 MyHashMap 类：
+ * MyHashMap() 用空映射初始化对象
+ * void put(int key, int value) 向 HashMap 插入一个键值对 (key, value) 。
+ * 如果 key 已经存在于映射中，则更新其对应的值 value 。
+ * int get(int key) 返回特定的 key 所映射的 value ；如果映射中不包含 key 的映射，返回 -1 。
+ * void remove(key) 如果映射中存在 key 的映射，则移除 key 和它所对应的 value 。
+ * <p>
+ * 示例：
+ * 输入：
+ * ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+ * [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+ * 输出：
+ * [null, null, null, 1, -1, null, 1, null, -1]
+ * 解释：
+ * MyHashMap myHashMap = new MyHashMap();
+ * myHashMap.put(1, 1); // myHashMap 现在为 [[1,1]]
+ * myHashMap.put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
+ * myHashMap.get(1);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+ * myHashMap.get(3);    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+ * myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+ * myHashMap.get(2);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+ * myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+ * myHashMap.get(2);    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+ * <p>
+ * 提示：
+ * 0 <= key, value <= 106
+ * 最多调用 104 次 put、get 和 remove 方法
+ * <p>
+ * 进阶：你能否不使用内置的 HashMap 库解决此问题？
+ */
+public class MyHashMap_706 {
+    private class Pair {
+        private int key, value;
+
+        public Pair(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    public MyHashMap_706() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; i++) {
+            data[i] = new LinkedList<Pair>();
+        }
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+
+    public void put(int key, int value) {
+        int hash = hash(key);
+        Iterator<Pair> iterator = data[hash].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                pair.setValue(value);
+                return;
+            }
+        }
+        data[hash].offerLast(new Pair(key, value));
+    }
+
+    public int get(int key) {
+        int hash = hash(key);
+        Iterator<Pair> iterator = data[hash].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                return pair.getValue();
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int hash = hash(key);
+        Iterator<Pair> iterator = data[hash].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                data[hash].remove(pair);
+                return;
+            }
+        }
+    }
+}
